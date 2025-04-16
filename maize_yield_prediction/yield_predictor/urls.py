ef938cd1b8ae056a92f3af8396c203993403c504
mosetf <stanlym315@gmail.com>
2025-04-16 16:26:07 +0300

    Add optimization view route to URLs and include landing page

--- maize_yield_prediction/yield_predictor/urls.py
+++ maize_yield_prediction/yield_predictor/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import predict_yield
+from .views import predict_yield, optimization_view
+from . import views
 
 urlpatterns = [
+    path('', views.landing_page, name='landing'),
     path('admin/', admin.site.urls),
     path('predict/', predict_yield, name='predict_yield'),
+    path('optimize/', optimization_view, name='optimize'),
+    # path('signup/', views.signup, name='signup'), 
 ]
