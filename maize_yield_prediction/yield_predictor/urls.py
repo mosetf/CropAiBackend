a79f73be93d2b8e5d60e256a26efbf4d7f007a04
mosetf <stanlym315@gmail.com>
2025-04-17 03:15:56 +0300

    Refactor URL patterns: remove unused optimization view and clean up imports

--- maize_yield_prediction/yield_predictor/urls.py
+++ maize_yield_prediction/yield_predictor/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import predict_yield, optimization_view
+from .views import predict_yield
 from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing'),
     path('admin/', admin.site.urls),
-    path('predict/', predict_yield, name='predict_yield'),
-    path('optimize/', optimization_view, name='optimize'),
-    # path('signup/', views.signup, name='signup'), 
+    path('predict/', views.predict_yield, name='predict_yield'),
 ]
