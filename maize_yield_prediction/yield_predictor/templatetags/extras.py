84bc55f45fdcfdeff488ee9e0000a4002070b8f4
mosetf <stanlym315@gmail.com>
2025-04-25 10:54:21 +0300

    Add 'avg' template filter to calculate average of a queryset field

--- maize_yield_prediction/yield_predictor/templatetags/extras.py
+++ maize_yield_prediction/yield_predictor/templatetags/extras.py
@@ -5,3 +5,8 @@ register = template.Library()
 @register.filter
 def pluck(objects, attr):
     return [getattr(obj, attr).isoformat() if hasattr(getattr(obj, attr), 'isoformat') else getattr(obj, attr) for obj in objects]
+
+@register.filter
+def avg(queryset, field):
+    values = [getattr(item, field) for item in queryset]
+    return sum(values) / len(values) if values else 0
