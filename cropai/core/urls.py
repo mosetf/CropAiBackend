ee8c2725fcd85de2009737e836129acade5fb869
Mosetf <stanlym315@gmail.com>
2026-03-30 02:22:12 +0300

    Add API schema and documentation endpoints using DRF Spectacular

--- cropai/core/urls.py
+++ cropai/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from yield_predictor.urls import api_urlpatterns
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/',    admin.site.urls),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/', include(api_urlpatterns)),
     path('',          include('yield_predictor.urls', namespace='yield_predictor')),
+    
+    # API Schema & Documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
