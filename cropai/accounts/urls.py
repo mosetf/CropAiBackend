605e254064c305666b5ca34c38a095ee9386eb95
Mosetf <stanlym315@gmail.com>
2026-03-30 02:20:41 +0300

    Add missing registration route to accounts API URLs

--- cropai/accounts/urls.py
+++ cropai/accounts/urls.py
@@ -7,6 +7,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('refresh/', views.refresh_view, name='refresh'),
