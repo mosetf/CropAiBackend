f14e901e0231a7290f87184d106e3a588a0806a2
Mosetf <stanlym315@gmail.com>
2026-03-29 15:47:10 +0300

    Refactor ASGI settings and enhance core settings with CORS and JWT configurations

--- cropai/core/urls.py
+++ cropai/core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from yield_predictor.urls import api_urlpatterns
 
 urlpatterns = [
     path('admin/',    admin.site.urls),
-    path('accounts/', include('allauth.urls')), 
+    path('accounts/', include('allauth.urls')),
+    path('api/v1/auth/', include('accounts.urls')),
+    path('api/v1/', include(api_urlpatterns)),
     path('',          include('yield_predictor.urls', namespace='yield_predictor')),
 ]
