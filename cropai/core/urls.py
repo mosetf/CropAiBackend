ebc5b30764b2132687265dce6c3af45c7c9239f8
Mosetf <stanlym315@gmail.com>
2026-04-19 18:25:33 +0300

    Add redirect to Swagger UI in URL patterns

--- cropai/core/urls.py
+++ cropai/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from yield_predictor.urls import api_urlpatterns
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/api/schema/swagger-ui/', permanent=False)),
     path('admin/',    admin.site.urls),
     path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/', include(api_urlpatterns)),
